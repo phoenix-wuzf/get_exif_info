66e95206b6f45de89d16ed41e31dc5c9bc4eafe7
phoenix-wuzf <wzf@wuzufei.cn>
2021-03-24 23:49:01 +0800

    commit get exif info source code

---
+++ src/com/wuzufei/sudo/Translate.java
@@ -0,0 +1,63 @@
+package com.wuzufei.sudo;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+public class Translate {
+
+    public static BufferedImage Mat2BufImg(Mat matrix, String fileExtension) {
+        // convert the matrix into a matrix of bytes appropriate for
+        // this file extension
+        MatOfByte mob = new MatOfByte();
+        Imgcodecs.imencode(fileExtension, matrix, mob);
+        // convert the "matrix of bytes" into a byte array
+        byte[] byteArray = mob.toArray();
+        BufferedImage bufImage = null;
+        try {
+            InputStream in = new ByteArrayInputStream(byteArray);
+            bufImage = ImageIO.read(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bufImage;
+    }
+
+    /**
+     * BufferedImage转换成Mat
+     *
+     * @param original 要转换的BufferedImage
+     * @param imgType  bufferedImage的类型 如 BufferedImage.TYPE_3BYTE_BGR
+     * @param matType  转换成mat的type 如 CvType.CV_8UC3
+     */
+    public static Mat BufImg2Mat(BufferedImage original, int imgType, int matType) {
+        if (original == null) {
+            throw new IllegalArgumentException("original == null");
+        }
+        if (original.getType() != imgType){
+            // Create a buffered image
+            BufferedImage image = new BufferedImage(original.getWidth(), original.getHeight(), imgType);
+            // Draw the image onto the new buffer
+            Graphics2D g = image.createGraphics();
+            try {
+                g.setComposite(AlphaComposite.Src);
+                g.drawImage(original, 0, 0, null);
+            } finally {
+                g.dispose();
+            }
+        }
+
+        byte[] pixels = ((DataBufferByte) original.getRaster().getDataBuffer()).getData();
+        Mat mat = Mat.eye(original.getHeight(), original.getWidth(), matType);
+        mat.put(0, 0, pixels);
+        return mat;
+    }
+}
